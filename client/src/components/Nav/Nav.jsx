8cb3764b948735198753a6f30dba76bb426b3bab
Aldinti <aldopati@gmail.com>
2023-06-10 21:13:34 -0500

    Nav

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { active, pending} from "./Nav.module.css";
+import { active, pending } from "./Nav.module.css";
 
 export const Nav = () => {
 	return (
@@ -8,7 +8,7 @@ export const Nav = () => {
 			<NavLink
 				to='/home'
 				className={({ isActive, isPending }) =>
-					isPending ? pending : isActive ? {active} : " "
+					isPending ? pending : isActive ? { active } : " "
 				}
 			>
 				Home
@@ -16,7 +16,7 @@ export const Nav = () => {
 			<NavLink
 				to='/activities'
 				className={({ isActive, isPending }) =>
-					isPending ? pending : isActive ? {active} : " "
+					isPending ? pending : isActive ? { active } : " "
 				}
 			>
 				Create activity
@@ -24,7 +24,7 @@ export const Nav = () => {
 			<NavLink
 				to='/about'
 				className={({ isActive, isPending }) =>
-					isPending ? pending : isActive ? {active} : " "
+					isPending ? pending : isActive ? { active } : " "
 				}
 			>
 				About
